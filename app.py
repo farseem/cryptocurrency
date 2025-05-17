88e484c1bf3390a9c5edb2ae9e2e31034cdc6a4f
Farsim <mail.farsim@gmail.com>
2025-05-18 00:30:42 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from tensorflow.keras.models import load_model
+import numpy as np
+
+app = Flask(__name__)
+model = load_model("my_model")  # Ensure this folder is included
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    features = np.array(data['features']).reshape(1, -1)
+    prediction = model.predict(features)[0][0]
+    return jsonify({'prediction': float(prediction)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
